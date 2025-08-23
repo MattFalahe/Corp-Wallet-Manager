fc49116c71addcd257ace2c4d3d76e52600dbc52
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-08-24 00:15:18 +0100

    Create corpwalletmanager.php

---
+++ src/Config/corpwalletmanager.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'refresh_interval' => 60000,
+    'color_actual' => '#4cafef',
+    'color_predicted' => '#ef4444',
+    'decimals' => 2,
+    'use_precomputed_predictions' => true,
+    'use_precomputed_monthly_balances' => true,
+];
