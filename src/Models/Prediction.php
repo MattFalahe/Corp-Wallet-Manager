90bbc347734cb6730a7f60058b59535e24d12331
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-08-24 13:36:11 +0100

    Update Prediction.php

--- src/Models/Prediction.php
+++ src/Models/Prediction.php
@@ -1,4 +1,5 @@
 <?php
+// Updated Prediction.php
 namespace Seat\CorpWalletManager\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Prediction extends Model
 {
     protected $table = 'corpwalletmanager_predictions';
-    protected $fillable = ['date','predicted_balance'];
+    
+    protected $fillable = [
+        'corporation_id',
+        'date',
+        'predicted_balance'
+    ];
+
+    protected $casts = [
+        'predicted_balance' => 'decimal:2',
+        'corporation_id' => 'integer',
+        'date' => 'date',
+    ];
+    
+    public function corporation()
+    {
+        return $this->belongsTo(\Seat\Eveapi\Models\Corporation\CorporationInfo::class, 'corporation_id', 'corporation_id');
+    }
 }
