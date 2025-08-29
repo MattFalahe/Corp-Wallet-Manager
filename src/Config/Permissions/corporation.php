bb809360a4448d41e1547f9a1cd52158db409400
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-08-29 05:49:36 +0100

    Update corporation.php

--- src/Config/Permissions/corporation.php
+++ src/Config/Permissions/corporation.php
@@ -1,19 +1,14 @@
 <?php
 
 return [
-    // Define the permissions your menu items reference
-    'wallet' => [
-        'view' => [
-            'label' => 'corpwalletmanager::permissions.wallet_view_label',
-            'description' => 'corpwalletmanager::permissions.wallet_view_description',
-            'division' => 'financial',
-        ],
+    'wallet_view' => [
+        'label' => 'corpwalletmanager::permissions.wallet_view_label',
+        'description' => 'corpwalletmanager::permissions.wallet_view_description',
+        'division' => 'financial',
     ],
-    'member' => [
-        'view' => [
-            'label' => 'corpwalletmanager::permissions.member_view_label',
-            'description' => 'corpwalletmanager::permissions.member_view_description',
-            'division' => 'financial',
-        ],
+    'member_view' => [
+        'label' => 'corpwalletmanager::permissions.member_view_label',
+        'description' => 'corpwalletmanager::permissions.member_view_description',
+        'division' => 'financial',
     ],
 ];
