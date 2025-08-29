f99d75f209bc1a974fb59ac6c1db3d2890f650be
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-08-29 05:29:25 +0100

    Create permissions.php

---
+++ src/resources/lang/en/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'wallet_view_label' => 'View Corporation Wallet (Director)',
+    'wallet_view_description' => 'Allows viewing detailed corporation wallet information and predictions',
+    'member_view_label' => 'View Corporation Wallet (Member)',
+    'member_view_description' => 'Allows viewing basic corporation wallet information',
+];
