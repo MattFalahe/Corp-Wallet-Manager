fc712d001c52fa3df5f83c3e9586ae69420a2b7f
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-08-30 17:10:23 +0100

    Update CorpWalletManagerServiceProvider.php

--- src/CorpWalletManagerServiceProvider.php
+++ src/CorpWalletManagerServiceProvider.php
@@ -22,6 +22,9 @@ class CorpWalletManagerServiceProvider extends AbstractSeatPlugin
         
         // CRITICAL: Register corporation menu items with SeAT
         $this->mergeConfigFrom(__DIR__ . '/Config/Menu/corporation.php', 'package.corporation.menu');
+
+        // Register permissions
+        $this->registerPermissions(__DIR__ . '/Config/Permissions/corporation.permissions.php', 'corporation');
                
         // Register commands
         $this->commands([
