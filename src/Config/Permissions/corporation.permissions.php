6218aa7e2297ca4eedd0fe675b7436ea401924c8
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-09-01 11:28:31 +0100

    Update corporation.permissions.php

--- src/Config/Permissions/corporation.permissions.php
+++ src/Config/Permissions/corporation.permissions.php
@@ -1,19 +1,20 @@
 <?php
 
 return [
-    'wallet_view' => [
-        'label' => 'corpwalletmanager::permissions.wallet_view_label',
-        'description' => 'corpwalletmanager::permissions.wallet_view_description',
-        'division' => 'financial',
+    'view' => [
+        'label' => 'corpwalletmanager::permissions.view_label',
+        'description' => 'corpwalletmanager::permissions.view_description',
+    ],
+    'director_view' => [
+        'label' => 'corpwalletmanager::permissions.director_view_label',
+        'description' => 'corpwalletmanager::permissions.director_view_description',
     ],
     'member_view' => [
         'label' => 'corpwalletmanager::permissions.member_view_label',
         'description' => 'corpwalletmanager::permissions.member_view_description',
-        'division' => 'financial',
     ],
-    'wallet_settings' => [
-        'label' => 'corpwalletmanager::permissions.wallet_settings_label',
-        'description' => 'corpwalletmanager::permissions.wallet_settings_description',
-        'division' => 'financial',
+    'settings' => [
+        'label' => 'corpwalletmanager::permissions.settings_label',
+        'description' => 'corpwalletmanager::permissions.settings_description',
     ],
 ];
