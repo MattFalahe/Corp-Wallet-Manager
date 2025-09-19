8d5ac3ae67c614db39ca43cacc0d203a5f1d1530
MattFalahe <47831385+MattFalahe@users.noreply.github.com>
2025-09-19 14:48:49 +0100

    Update and rename src/Http/Controllers/Api/InternalTransferApiController.php to src/Http/Controllers/InternalTransferApiController.php

--- src/Http/Controllers/InternalTransferApiController.php
+++ src/Http/Controllers/InternalTransferApiController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Seat\CorpWalletManager\Http\Controllers\Api;
+namespace Seat\CorpWalletManager\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
